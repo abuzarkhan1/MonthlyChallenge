5d2401549e5e2cd4f739dae137e7c5811d5014cd
Abuzar <abuzarkhan1242@gmail.com>
2025-04-22 00:46:44 +0500

    add challenge done

--- src/main/java/com/abuzar/CrudSpringBoot/Controller/ChallengeController.java
+++ src/main/java/com/abuzar/CrudSpringBoot/Controller/ChallengeController.java
@@ -3,6 +3,7 @@ package com.abuzar.CrudSpringBoot.Controller;
 import com.abuzar.CrudSpringBoot.Challenge;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,12 +16,16 @@ public class ChallengeController {
 
     public ChallengeController() {
         challenges.add(new Challenge(1, "Challenge 1", "Description for challenge 1"));
-        challenges.add(new Challenge(2, "Challenge 2", "Description for challenge 2"));
-        challenges.add(new Challenge(3, "Challenge 3", "Description for challenge 3"));
     }
 
     @GetMapping("/challenges")
     public List<Challenge> getAllChallenges() {
         return challenges;
+    }
+
+    @PostMapping("/challenges")
+    public String addChallenge(@RequestBody Challenge challenge) {
+        challenges.add(challenge);
+        return "Challenge added successfully";
     }
 }
