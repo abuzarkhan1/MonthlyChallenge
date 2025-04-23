826049e74b43b92d37d96f89cc09bacdd1611ceb
Abuzar <abuzarkhan1242@gmail.com>
2025-04-23 22:30:26 +0500

    now frontend in react

--- Backend/src/main/java/com/abuzar/CrudSpringBoot/Controller/ChallengeController.java
+++ Backend/src/main/java/com/abuzar/CrudSpringBoot/Controller/ChallengeController.java
@@ -2,7 +2,6 @@ package com.abuzar.CrudSpringBoot.Controller;
 
 import com.abuzar.CrudSpringBoot.Entities.Challenge;
 import com.abuzar.CrudSpringBoot.Service.ChallengeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
